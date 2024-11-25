a2d594f2883666d767311a0ae366863c9842ee78
topdev113 <topdev367@gmail.com>
2024-11-25 12:48:02 +0000

    first push

---
+++ CHANGELOG.md
@@ -0,0 +1,94 @@
+<!--
+Guiding Principles:
+
+Changelogs are for humans, not machines.
+There should be an entry for every single version.
+The same types of changes should be grouped.
+Versions and sections should be linkable.
+The latest version comes first.
+The release date of each version is displayed.
+Mention whether you follow Semantic Versioning.
+
+Usage:
+
+Change log entries are to be added to the Unreleased section under the
+appropriate stanza (see below). Each entry should have following format:
+
+* [#PullRequestNumber](PullRequestLink) message
+
+Types of changes (Stanzas):
+
+"Features" for new features.
+"Improvements" for changes in existing functionality.
+"Deprecated" for soon-to-be removed features.
+"Bug Fixes" for any bug fixes.
+"Client Breaking" for breaking CLI commands and REST routes used by end-users.
+"API Breaking" for breaking exported APIs used by developers building on SDK.
+"State Machine Breaking" for any changes that result in a different AppState
+given same genesisState and txList.
+Ref: https://keepachangelog.com/en/1.0.0/
+-->
+
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+
+## Unreleased
+
+## v0.17.1
+
+### Improvements
+
+* [#113](https://github.com/babylonlabs-io/vigilante/pull/113) goreleaser wasm version
+* [#115](https://github.com/babylonlabs-io/vigilante/pull/115) updates bbn to v0.17.1
+
+
+## v0.17.0
+
+### Improvements
+
+* [#100](https://github.com/babylonlabs-io/vigilante/pull/100) bump docker workflow to 0.10.2,
+fix some dockerfile issue
+* [#111](https://github.com/babylonlabs-io/vigilante/pull/111) updates bbn to v0.17.0
+
+## v0.16.1
+
+### Improvements
+
+* [#105](https://github.com/babylonlabs-io/vigilante/pull/105) Measure latency
+* [#106](https://github.com/babylonlabs-io/vigilante/pull/106) Wait for stacking tx to be k-deep
+
+
+## v0.16.0
+
+* [#94](https://github.com/babylonlabs-io/vigilante/pull/94) adds gosec and fixes gosec issues
+* [#96](https://github.com/babylonlabs-io/vigilante/pull/96) fixes potential stale data read
+* [#98](https://github.com/babylonlabs-io/vigilante/pull/98) fixes golangci configuration
+* [#102](https://github.com/babylonlabs-io/vigilante/pull/102) babylon v0.16.0 upgrade
+
+## v0.15.0
+
+* [#90](https://github.com/babylonlabs-io/vigilante/pull/90) upgrade babylon to v0.15.0
+
+## v0.14.0
+
+### Bug Fixes
+
+* [#84](https://github.com/babylonlabs-io/vigilante/pull/84) fix spawning more go routines than needed when activating 
+delegations, add more logging
+
+### Improvements
+* [#87](https://github.com/babylonlabs-io/vigilante/pull/87) adr 029 for generalized unbonding
+
+## v0.13.0
+
+### Improvements
+
+* [#80](https://github.com/babylonlabs-io/vigilante/pull/80) bump babylon to use
+uint32 in BTC block heights and BLS valset response
+* [#79](https://github.com/babylonlabs-io/vigilante/pull/79) handle no change output when building tx
+* [#77](https://github.com/babylonlabs-io/vigilante/pull/77) add arm64 static build
+* [#76](https://github.com/babylonlabs-io/vigilante/pull/76) add goreleaser
+  setup and move out changelog reminder
---
+++ btcstaking-tracker/stakingeventwatcher/tracked_delegations.go
@@ -0,0 +1,151 @@
+package stakingeventwatcher
+
+import (
+	"fmt"
+	"iter"
+	"sync"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
+)
+
+type TrackedDelegation struct {
+	StakingTx             *wire.MsgTx
+	StakingOutputIdx      uint32
+	UnbondingOutput       *wire.TxOut
+	DelegationStartHeight uint32
+	ActivationInProgress  bool
+}
+
+type TrackedDelegations struct {
+	mu sync.RWMutex
+	// key: staking tx hash
+	mapping map[chainhash.Hash]*TrackedDelegation
+}
+
+func NewTrackedDelegations() *TrackedDelegations {
+	return &TrackedDelegations{
+		mapping: make(map[chainhash.Hash]*TrackedDelegation),
+	}
+}
+
+// GetDelegation returns the tracked delegation for the given staking tx hash or nil if not found.
+func (td *TrackedDelegations) GetDelegation(stakingTxHash chainhash.Hash) *TrackedDelegation {
+	td.mu.RLock()
+	defer td.mu.RUnlock()
+
+	del, ok := td.mapping[stakingTxHash]
+
+	if !ok {
+		return nil
+	}
+
+	return del
+}
+
+// GetDelegations returns all tracked delegations as a slice.
+func (td *TrackedDelegations) GetDelegations() []*TrackedDelegation {
+	td.mu.RLock()
+	defer td.mu.RUnlock()
+
+	// Create a slice to hold all delegations
+	delegations := make([]*TrackedDelegation, 0, len(td.mapping))
+
+	// Iterate over the map and collect all values (TrackedDelegation)
+	for _, delegation := range td.mapping {
+		delegations = append(delegations, delegation)
+	}
+
+	return delegations
+}
+
+func (td *TrackedDelegations) DelegationsIter() iter.Seq[*TrackedDelegation] {
+	return func(yield func(*TrackedDelegation) bool) {
+		td.mu.RLock()
+		defer td.mu.RUnlock()
+
+		// we lock for the entirety of the iteration
+		for _, v := range td.mapping {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+
+func (td *TrackedDelegations) AddDelegation(
+	stakingTx *wire.MsgTx,
+	stakingOutputIdx uint32,
+	unbondingOutput *wire.TxOut,
+	delegationStartHeight uint32,
+	shouldUpdate bool,
+) (*TrackedDelegation, error) {
+	delegation := &TrackedDelegation{
+		StakingTx:             stakingTx,
+		StakingOutputIdx:      stakingOutputIdx,
+		UnbondingOutput:       unbondingOutput,
+		DelegationStartHeight: delegationStartHeight,
+	}
+
+	stakingTxHash := stakingTx.TxHash()
+
+	td.mu.Lock()
+	defer td.mu.Unlock()
+
+	if _, ok := td.mapping[stakingTxHash]; ok {
+		if shouldUpdate {
+			// Update the existing delegation
+			td.mapping[stakingTxHash] = delegation
+			return delegation, nil
+		}
+		return nil, fmt.Errorf("delegation already tracked for staking tx hash %s", stakingTxHash)
+	}
+
+	td.mapping[stakingTxHash] = delegation
+	return delegation, nil
+}
+
+func (td *TrackedDelegations) RemoveDelegation(stakingTxHash chainhash.Hash) {
+	td.mu.Lock()
+	defer td.mu.Unlock()
+
+	delete(td.mapping, stakingTxHash)
+}
+
+func (td *TrackedDelegations) HasDelegationChanged(
+	stakingTxHash chainhash.Hash,
+	newDelegation *newDelegation,
+) (bool, bool) {
+	td.mu.Lock()
+	defer td.mu.Unlock()
+
+	// Check if the delegation exists in the map
+	existingDelegation, exists := td.mapping[stakingTxHash]
+	if !exists {
+		// If it doesn't exist, return false for changed, and false for exists
+		return false, false
+	}
+
+	// Compare height to check if the delegation has changed
+	if existingDelegation.DelegationStartHeight != newDelegation.delegationStartHeight {
+		return true, true // The delegation has changed and it exists
+	}
+
+	// The delegation exists but hasn't changed
+	return false, true
+}
+
+func (td *TrackedDelegations) UpdateActivation(tx chainhash.Hash, inProgress bool) error {
+	td.mu.Lock()
+	defer td.mu.Unlock()
+
+	delegation, ok := td.mapping[tx]
+
+	if !ok {
+		return fmt.Errorf("delegation with tx hash %s not found", tx.String())
+	}
+
+	delegation.ActivationInProgress = inProgress
+
+	return nil
+}
